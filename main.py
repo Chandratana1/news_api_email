1910f4a32eb27deea6d0cc1f0de45af17357bb80
Chandra Tanabuddi <test@test.com>
2024-10-07 18:19:54 -0600

    Enabled dict and got the content of one attribute

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import requests
 
-ap_key = "a5d6283b412b4f86b010490b59434918"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-09-08&" \
-       "sortBy=publishedAt& " \
-       "apiKey=a5d6283b412b4f86b010490b59434918 "
+api_key = "a5d6283b412b4f86b010490b59434918"
+url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-09-08"
+       "&sortBy=publishedAt&"
+       "apiKey=a5d6283b412b4f86b010490b59434918")
 
+
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+#Make a dict using json
+content = request.json()
+
+#  Access the article titles and description
+for article in content["articles"]:
+       print(article["title"])
+# print(content["articles"])
+
+print(type(content))
